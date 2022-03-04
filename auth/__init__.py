d45d0245279e1c2d5a04176b49614794db7f5f77
Albert Weihermann <albert.weihermann@hotmail.com>
2022-03-03 21:17:50 -0300

    Create simple form flow to login

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sassutils.wsgi import SassMiddleware
 
 app = Flask(__name__)
@@ -7,6 +7,22 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
     "auth": ('static/sass', 'static/css', '/static/css')
 })
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+def validate_authentication(email, password):
+    return email == "test.email@domain.com" and password == "123"
+
+@app.route("/", methods=['GET', 'POST'])
+def auth_page():
+    if request.method == 'GET': 
+        return render_template("login.html")
+    
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    if not email or not password:
+        return render_template("login.html", msg_error="E-mail and Password are required to login in platform.")
+    
+    if validate_authentication(email, password):
+        return render_template("welcome.html")
+    
+    return render_template("login.html", msg_error="E-mail and Password are wrong, please try again.")    
+    
